68b005b259ef6af56a692f8e208f43abc8b27361
clonidine <lcly@duck.com>
2024-01-04 14:55:43 -0300

    feat(lib.rs): implemented Repository for UserRepository struct

--- back/lib/src/repository/user.rs
+++ back/lib/src/repository/user.rs
@@ -1,1 +1,15 @@
+use crate::domain::user::User;
+
+use super::Repository;
+
 pub struct UserRepository;
+
+impl Repository<User> for UserRepository {
+    fn find_one(id: u64) -> User {
+        todo!()
+    }
+
+    fn find_all() -> Vec<User> {
+        todo!()
+    }
+}
