f33d0254d53a038979db593377bd47afbbbb9cc2
clonidine <lcly@duck.com>
2024-01-19 16:02:43 -0300

    refactor: create 'crates' folder and move crates

--- back/crates/zydus-core/src/repository/user.rs
+++ back/crates/zydus-core/src/repository/user.rs
@@ -14,3 +14,4 @@ impl Repository<User> for UserRepository {
         todo!()
     }
 }
+
---
+++ back/crates/zydus-http/src/lib.rs
@@ -0,0 +1,9 @@
+pub struct Server {
+    port: u16,
+}
+
+impl Server {
+    fn start(&self) -> Result<(), String> {
+        todo!()
+    }
+}
